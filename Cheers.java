cfd400578ad8914e465a4ec60e03c8dc61127021
Mor Cohen <mor.cohen02@post.runi.ac.il>
2024-11-15 16:52:18 +0200

    try

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,28 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String cheering = args[0];
+        int n = Integer.parseInt(args[1]);
+        String an = "AEFHILMNORSX";
+        boolean is = false;
+        for (int i=0; i<12; i++){
+                if(an.charAt(i) == cheering.charAt(0))
+                {
+                        is = true;
+                        break;
+                }
+        }
+
+        
+        for (int i=0; i<n; i++)
+        {
+                if (is)
+                {
+                        System.out.println("");
+                }
+                
+        }
+
+        
         }
 }
